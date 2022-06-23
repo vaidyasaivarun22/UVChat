2cd6698fa94a31e0b53638e2f392c86e94612446
vaidyasaivarun22 <vaisyasaivarun22@gmail.com>
2022-06-24 00:46:39 +0530

    added automatic scroll feature on message update

--- public/js/main.js
+++ public/js/main.js
@@ -12,25 +12,33 @@ const append=(message,position)=>{
     messageContainer.append(messageElement);
 }
 
-const userName = prompt("Enter your name before you join the party!!!");
-socket.emit('new-user-joined',userName);
+    userName = prompt("Enter your name before you join the party!!!");
+    socket.emit('new-user-joined',userName);
 
 socket.on('user-joined',name=>{
     append(`${name} joined the chat`,'center');
 })
 
+// This event fires when it receives a broadcasted message from the user 
 socket.on('receive',data=>{
     append(`${data.name} : ${data.message}`,'left');
+    messageContainer.scrollTop = messageContainer.scrollHeight;
 })
 
+// This event fire when it receives disconnnect signal from server .
 socket.on('left',name=>{
     append(`${name} left the chat`,'center');
 })
 
+// After sending my own message it is appended as follows on right .
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const message = messageInput.value;
-    append(`You : ${message}`,'right');
-    socket.emit('send',message);
-    messageInput.value = '';
+    if(message !== '')
+    {
+        append(`You : ${message}`,'right');
+        socket.emit('send',message);
+        messageContainer.scrollTop = messageContainer.scrollHeight;
+        messageInput.value = '';
+    }
 })
--- server.js
+++ server.js
@@ -8,26 +8,26 @@ require('dotenv').config();
 const server = http.createServer(app);
 const io = socketio(server); 
 
-// set static folder
+// set static folder to connect between the server and the frontend 
 app.use(express.static(path.join(__dirname,'public')));
 
-// runs when client connects
 const users={};
 
+// runs when client connects
 io.on('connection',socket=>{
 
     socket.on('new-user-joined',name =>{
         // console.log(name);
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined',name);
+        socket.broadcast.emit('user-joined',name);//This is the message sent to all the users except the one that has joined.(broadcast for reason used)
     });
 
     socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]}) //This is the message sent to all the users except the one that has messagged.
     });
 
     socket.on('disconnect', message=>{
-        socket.broadcast.emit('left',users[socket.id]);
+        socket.broadcast.emit('left',users[socket.id]); //This is the message sent to all the users except the one that has left.
         delete users[socket.id];
     })
 })
