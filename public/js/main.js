0aa99daf0a725fdf2d11535692c9dd03e09dbd1d
vaidyasaivarun22 <vaisyasaivarun22@gmail.com>
2022-06-23 20:11:22 +0530

    v-1.0

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+const socket = io();
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container');
+
+const append=(message,position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
+const userName = prompt("Enter your name before you join the party!!!");
+socket.emit('new-user-joined',userName);
+
+socket.on('user-joined',name=>{
+    append(`${name} joined the chat`,'center');
+})
+
+socket.on('receive',data=>{
+    append(`${data.name} : ${data.message}`,'left');
+})
+
+socket.on('left',name=>{
+    append(`${name} left the chat`,'center');
+})
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You : ${message}`,'right');
+    socket.emit('send',message);
+    messageInput.value = '';
+})
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path= require('path');
+const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketio(server); 
+
+// set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+// runs when client connects
+const users={};
+
+io.on('connection',socket=>{
+
+    socket.on('new-user-joined',name =>{
+        // console.log(name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined',name);
+    });
+
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+    });
+
+    socket.on('disconnect', message=>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    })
+})
+
+
+
+
+PORT = 3000 || process.env.PORT;
+
+server.listen(PORT,()=>{
+    console.log(`server running on PORT ${PORT}`);
+})
